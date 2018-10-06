994eb78462e55931f4a61acf1c26494b69b46510
Facundo Mariano Rivas <42252816+FakuProphet@users.noreply.github.com>
2018-10-06 18:48:16 -0300

    comentario

--- WindowsFormTurnos/Form1.cs
+++ WindowsFormTurnos/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormTurnos
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // evento load 
+        }
     }
 }
