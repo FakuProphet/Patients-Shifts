42d7875ca14141234acab6386bbf1a24ee4f4042
Facundo Mariano Rivas <42252816+FakuProphet@users.noreply.github.com>
2018-10-07 15:31:03 -0300

    metodos de reloj y fecha formulario principal.

--- WindowsFormTurnos/Form1.cs
+++ WindowsFormTurnos/Form1.cs
@@ -15,17 +15,32 @@ namespace WindowsFormTurnos
     {
 
         Conexion miConexion;
+        private Timer tiempo;
 
         public Form1()
         {
             InitializeComponent();
+            tiempo = new Timer();
+            tiempo.Tick += new EventHandler(eventoReloj);
+            tiempo.Enabled = true;
             miConexion = new Conexion();
+        }
+
+        private void eventoReloj(object sender, EventArgs e)
+        {
+            DateTime hoy = DateTime.Now;
+            lblHoraActual.Text = hoy.ToString("H:mm");
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
             miConexion.OpenConnection();
+            fechaActual();
+        }
+
+        private void fechaActual()
+        {
+            txtFecha.Text = DateTime.Today.ToShortDateString();
         }
     }
 }
