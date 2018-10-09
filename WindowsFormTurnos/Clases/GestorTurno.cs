97577fae012164a6cea71c0d20f9c135a7383e12
Facundo Mariano Rivas <42252816+FakuProphet@users.noreply.github.com>
2018-10-09 11:06:54 -0300

    limpieza codigo

--- WindowsFormTurnos/Clases/GestorTurno.cs
+++ WindowsFormTurnos/Clases/GestorTurno.cs
@@ -1,12 +1,13 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+
+
 using WindowsFormTurnos.AccesoBBDD;
+
+
 
 namespace WindowsFormTurnos.Clases
 {
